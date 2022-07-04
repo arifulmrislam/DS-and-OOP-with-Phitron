4afd6c653ae6270b9f8adcf160d187ed13c3f8a5
arifulmrislam <ariful.mr.islam@gmail.com>
2022-07-05 02:32:34 +0300

    DS and OOP

---
+++ Module 2.5 week 01 practice day 01/2.5.1 condition.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace  std;
+
+int main()
+{
+//    int x;
+//    cin>>x;
+//    if(x%2==0)
+//    {
+//        if(x<10)
+//        {
+//            cout<<"Even and less than 10"<<endl;
+//        }
+//        else
+//        {
+//            cout<<"Even and greater than equal 10"<<endl;
+//        }
+//    }
+//    else
+//    {
+//        cout<<"Odd"<<endl;
+//    }
+
+    char c;
+    cin>>c;
+
+    switch(c){
+    case 'a':
+        cout<<"Vowel"<<endl;
+        break;
+    case 'e':
+        cout<<"Vowel"<<endl;
+        break;
+    case 'i':
+        cout<<"Vowel"<<endl;
+        break;
+    case 'o':
+        cout<<"Vowel"<<endl;
+        break;
+    case 'u':
+        cout<<"Vowel"<<endl;
+        break;
+    default:
+        cout<<"Consonant"<<endl;
+    }
+
+    return 0;
+}
---
+++ Module 2.5 week 01 practice day 01/2.5.2 Loops.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+//    cin>>n;
+//    int sum=0;
+//    for(int i=1;i<=n;i++){
+//        cout <<" "<<i<<endl;
+//        sum+=i;
+//    }
+//    cout<<"Total:"<<sum<<endl;
+    cin>>n;
+    while(n<=10){
+        cout<<n<<" "<<endl;
+        n++;
+    }
+    return 0;
+}
